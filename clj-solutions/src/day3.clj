47200be6026a14bedd3af5fcd3f6aac1f7fb4b77
Sam Mussell <smussell@apple.com>
2022-12-03 18:56:13 -0800

    day 3

---
+++ clj-solutions/src/day3.clj
@@ -0,0 +1,62 @@
+(ns day3
+  (:use util)
+  (:require [clojure.string :as str])
+  (:require [clojure.set :as set]))
+
+(def should-run-mocks false)
+
+(defn process-input [input]
+  (->> input
+      str/trim
+      split-lines))
+
+(defn to-value [ch]
+  (let [n (int ch)]
+    (if (< n 91) 
+      (- n 38) 
+      (- n 96))))
+
+(defn split-half [seq] 
+  (split-at (/ (count seq) 2) seq))
+
+(defn part1 [input] 
+  (->> input
+       process-input
+       (map seq)
+       (map split-half)
+       (map #(map set %))
+       (map #(apply set/intersection %))
+       (map #(to-value (first %)))
+       sum))
+
+(def sample-input "
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+")
+
+(run-test part1 "day3Input.txt" should-run-mocks [
+  {
+    :input sample-input
+    :output "157"
+  }
+])
+
+(defn part2 [input] 
+  (->> input
+       process-input
+       (partition 3)
+       (map #(map set %))
+       (map #(apply set/intersection %))
+       (map #(to-value (first %)))
+       sum))
+
+(run-test part2 "day3Input.txt" should-run-mocks [
+  {
+    :input sample-input
+    :output "70"
+  }
+])
