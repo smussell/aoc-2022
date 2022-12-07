226880591c0f91a6cb4efb0d14a99b0dd488805c
Sam Mussell <smussell@apple.com>
2022-12-06 21:15:42 -0800

    day 6

---
+++ clj-solutions/src/day6.clj
@@ -0,0 +1,47 @@
+(ns day6
+  (:use util)
+  (:require [clojure.string :as str]))
+
+(def should-run-mocks false)
+
+(defn process-input [input]
+  (->> input
+       str/trim
+       seq))
+
+(defn exec [len input]
+  (loop [list input 
+         ind 0]
+    (let [item-set (set (take len list))]
+      (if (= len (count item-set)) (+ len ind)
+          (recur (rest list) (inc ind))))))
+
+(defn part1 [input] 
+  (->> input 
+       process-input
+       (exec 4)
+       debug))
+
+(def sample-input "
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+")
+
+(run-test part1 "day6Input.txt" should-run-mocks [
+  {
+   :input sample-input
+   :output "7"
+   }
+])
+
+(defn part2 [input] 
+  (->> input 
+       process-input
+       (exec 14)
+       debug))
+
+(run-test part2 "day6Input.txt" should-run-mocks [
+  {
+    :input sample-input
+    :output "19"
+  }
+])
